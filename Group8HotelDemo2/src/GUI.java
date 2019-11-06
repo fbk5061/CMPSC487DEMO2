f8241c1f1feedf513f45cfc5050fcd22d8957d40
samuel <samuel.qtown@gmail.com>
2019-11-06 16:41:22 -0500

    SWITCH to Java Swing

---
+++ Group8HotelDemo2/src/GUI.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI {
+    private JTextField searchTextField; //Text read to user, DO NOT EDIT
+    private JTextField textField2; //User Input
+    private JButton searchButton;
+    private JTextArea textArea1;
+
+    public GUI() {
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+}
